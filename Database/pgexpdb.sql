12b8a925b142596a46c52ef5910caadf264b2f97
AAYUSHTAILANG <tailangaayu@gmail.com>
2024-02-08 08:56:01 +0530

    databases updated

--- Database/pgexpdb.sql
+++ Database/pgexpdb.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `area`
 --
 
+CREATE DATABASE  IF NOT EXISTS `pgexp`
+
 DROP TABLE IF EXISTS `area`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
